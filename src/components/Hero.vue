021a52274a727f588e4a474734faec90abac246c
Tigran <harutyunyan.tigran1975@gmail.com>
2024-01-19 18:49:02 +0400

    added Hero section

---
+++ src/components/Hero.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const handleClick = () => {
+  window.open("https://github.com/");
+};
+</script>
+
+<template>
+  <header class="w-full flex justify-center items-center flex-col">
+    <nav class="flex justify-between items-center w-full mb-10 pt-3">
+      <img src="/images/logo.svg" alt="sumz_logo" class="w-28 object-contain" />
+
+      <button type="button" @click="handleClick()" class="black_btn">
+        Github
+      </button>
+    </nav>
+
+    <h1 class="head_text">
+      Summarize Articles with <br class="max-md:hidden" />
+      <span class="orange_gradient">OpenAI GPT-4</span>
+    </h1>
+
+    <h2 class="desc">
+      Simply your reading links, an open-source article summarizer that
+      transforms lengthy articles into clear and consise summaries
+    </h2>
+  </header>
+</template>
